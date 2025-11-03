8d57705a2101e687724fe7f7815f92fabac7e8d4
quel-exe <qexequel@gmail.com>
2025-11-03 17:41:37 +0800

    properties calling backend for data

--- tenancy_system/properties/views.py
+++ tenancy_system/properties/views.py
@@ -3,6 +3,6 @@ from .models import Property
 from .serializers import PropertySerializer
 
 class PropertyViewSet(viewsets.ModelViewSet):
-    queryset = Property.objects.all()
+    queryset = Property.objects.all().order_by('-date_added')
     serializer_class = PropertySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]  # Change to IsAuthenticated once login works
