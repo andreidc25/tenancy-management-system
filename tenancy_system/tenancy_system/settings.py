a21e98e3a660b9dd7d17d2dd36e4a6c87adfa76a
mrabaajr <mrabaajr@tip.edu.ph>
2025-11-12 19:49:23 +0800

    Added 127.0.0.1 to allowed hosts

--- tenancy_system/tenancy_system/settings.py
+++ tenancy_system/tenancy_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(^^-2=27o@t9zwoq_jk^o73sw+(^y25cix*an&68=*c)fl2ks9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
